ea9f23ce66f4a95742a9a1e43530b8232ad14120
brenoaug <breaugustocp@outlook.com>
2024-12-11 19:12:16 -0300

    exercicio_pares feito

---
+++ 06-arrays/exercicio_pares.js
@@ -0,0 +1,8 @@
+const todosNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let i = 0; i < todosNumeros.length; i++){
+  let numero = todosNumeros[i];
+  if (numero % 2  === 0){
+    console.log(numero);
+  }
+}
--- 06-arrays/exercicio_tabuada.js
+++ 06-arrays/exercicio_tabuada.js
@@ -1,6 +1,24 @@
 const numero = 7;
+let i = 1;
 
-for (let i = 1 ; i <= 10; i++){
+//usando o for
+/*for (let i = 1 ; i <= 10; i++){
     resultadoMultiplicacao = numero * i;
     console.log(`${i} x ${numero} = ${resultadoMultiplicacao}`);
-}
+}*/
+
+//usando o while
+/*
+while (i<=10){
+    resultadoMultiplicacao = numero * i;
+    console.log(`${i} x ${numero} = ${resultadoMultiplicacao}`);
+    i++;
+}*/
+
+//usando do while
+do {
+    resultadoMultiplicacao = numero * i;
+    console.log(`${i} x ${numero} = ${resultadoMultiplicacao}`);
+    i++;
+}while (i<=10);
+
